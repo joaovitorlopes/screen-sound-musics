710f7c0687889bef9e6b028b0548089a6f19fc57
joaovitorlopes <jvlaaa+github@gmail.com>
2024-05-24 19:22:10 -0400

    first commit

--- src/main/java/joaovitorlopes/com/github/screensoundmusics/ScreensoundmusicsApplication.java
+++ src/main/java/joaovitorlopes/com/github/screensoundmusics/ScreensoundmusicsApplication.java
@@ -1,13 +1,20 @@
 package joaovitorlopes.com.github.screensoundmusics;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import joaovitorlopes.com.github.screensoundmusics.main.Main;
 
 @SpringBootApplication
-public class ScreensoundmusicsApplication {
+public class ScreensoundmusicsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreensoundmusicsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.showMenu();
+	}
 }
--- src/main/java/joaovitorlopes/com/github/screensoundmusics/main/Main.java
+++ src/main/java/joaovitorlopes/com/github/screensoundmusics/main/Main.java
@@ -1,5 +1,73 @@
 package joaovitorlopes.com.github.screensoundmusics.main;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private Scanner reading = new Scanner(System.in);
+
+    public void showMenu() {
+        var option = -1;
+
+        while (option != 9) {
+            var menu = """
+                    *** Screen Sound Music ***
+                                        
+                    1- Register Artists
+                    2- Register Musics
+                    3- List Musics
+                    4- Search Musics By Artists
+                    5- Search About Artists
+                                    
+                    9 - Exit
+                    """;
+
+            System.out.println(menu);
+            option = reading.nextInt();
+            reading.nextLine();
+
+            switch (option) {
+                case 1:
+                    registerArtists();
+                    break;
+                case 2:
+                    registerMusics();
+                    break;
+                case 3:
+                    listMusics();
+                    break;
+                case 4:
+                    searchMusicsByArtists();
+                    break;
+                case 5:
+                    searchAboutArtists();
+                    break;
+                case 9:
+                    System.out.println("Exiting...!");
+                    break;
+                default:
+                    System.out.println("Invalid Entry!");
+            }
+        }
+    }
+
+    private void registerArtists() {
+
+    }
+
+    private void listMusics() {
+
+    }
+
+    private void searchMusicsByArtists() {
+
+    }
+
+    private void searchAboutArtists() {
+
+    }
+
+    private void registerMusics() {
+
+    }
 }
